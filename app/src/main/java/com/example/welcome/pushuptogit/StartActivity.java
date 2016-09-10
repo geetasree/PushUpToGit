b7765df327aec7361a60231a2991e2478c4d5324
Welcome <Welcome>
2016-09-10 18:03:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/welcome/pushuptogit/StartActivity.java
@@ -0,0 +1,17 @@
+package com.example.welcome.pushuptogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void testUpload(){
+        int  i=55;
+    }
+}
